ab2674861d32fd256476b0153ffa6a1c1da1cc50
Lior <agraded@gmail.com>
2019-12-30 13:27:22 +0200

    recycleview

--- Controller/WishList.py
+++ Controller/WishList.py
@@ -1,19 +1,27 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.recycleview import RecycleView
 from kivy.uix.widget import Widget
 
 from DataObjects.Post import Post
 
+
 posts = []
 posts.append(Post("1", "admin", "BMW", "2010", "Blue", "110,000", "BLOB", "550", "New car available!"))
 
-class WishList(Widget):
+class RecycleWishlist(RecycleView):
 
-    def __init__(self, postsLiked):
-        super(WishList, self).__init__()
-        self.add_widget(Label(text= "Yoink", pos_hint={"y": 0.5, "x" : 0.5}, size= (500, 500)))
+    def __init__(self, **kwargs):
+        super(RecycleWishlist, self).__init__(**kwargs)
+        self.data = [{'text': str(x), 'on_press': self.btnPressed(x)} for x in range (20)]
+
+    def btnPressed(self, numPressed):
+        print ("Pressed " + str(numPressed))
+
 
         # for post in postsLiked:
         #     label = Label(text = post.model + " ," + post.color + " - " + post.price +"$")
@@ -23,7 +31,7 @@ class WishList(Widget):
 
 class MyApp(App):
     def build(self):
-        return WishList(posts)
+        return RecycleWishlist()
 
 if __name__ == "__main__":
     MyApp().run()
