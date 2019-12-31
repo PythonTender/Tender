1249f862963f27282d669afd4805022ec4c63865
Lior <agraded@gmail.com>
2019-12-31 21:43:18 +0200

    Register tweeks

--- Controller/Register.py
+++ Controller/Register.py
@@ -1,4 +1,4 @@
-from View import db
+from Model.DBConnection import DBConnection
 import kivy
 from kivy.properties import ObjectProperty
 
@@ -10,6 +10,7 @@ class Register:
     location= ObjectProperty(None)
 
     def register(self):
+        db = DBConnection()
         db.createUser(self.username.text, self.password.text, self.firstName.text, self.lastName.text, self.location.text)
         return True
 
