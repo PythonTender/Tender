e8a2c5eac091ddcc14a526d7def7dfa9c9fa249c
Lior <agraded@gmail.com>
2020-01-03 22:09:39 +0200

    Menubar widget added

--- TenderApp.py
+++ TenderApp.py
@@ -1,3 +1,6 @@
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+
 from Controller.PostView import PostView
 from Controller.WishListView import RecycleWishlist
 from Controller.WishList import WishList
@@ -11,15 +14,20 @@ from Model.WishListModel import WishListModel
 
 LabelBase.register(name='Gotham Rounded',fn_regular='resources/fonts/Gotham Rounded Light.otf',fn_bold='resources/fonts/Gotham Rounded Bold.otf')
 
+#
+# class MenuBar(GridLayout):
+#     pass
 
 class TenderApp(App):
     def __init__(self):
         App.__init__(self)
 
+
     def build(self):
-        #return PostView(model=PostViewModel(DBModel()))
-        return WishList(model=WishListModel(DBModel()))
-        # return RecycleWishlist()
+        # return PostView(model=PostViewModel(DBModel()))
+        # return WishList(model=WishListModel(DBModel()))
+        return RecycleWishlist()
+        # return MenuBar()
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
