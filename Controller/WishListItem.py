e86edcdc4b5dcf701265aaf5541904e4b633a0e7
lioryako <1>
2020-01-09 21:16:07 +0200

    FUCK DIS

--- Controller/WishListItem.py
+++ Controller/WishListItem.py
@@ -1,7 +1,9 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.properties import ObjectProperty
-from Controller import PostDetailedView
+from kivy.uix.button import Button
+
+from Controller.PostDetailedView import PostDetailedView
 from kivy.uix.popup import Popup
 
 
@@ -11,15 +13,15 @@ class WishListItem(BoxLayout):
     Post = ObjectProperty(None)
 
     def __init__(self, **kwargs):
-        super(WishListItem, self).__init__()
+        super(WishListItem, self).__init__(**kwargs)
         self.Post = kwargs.get('post',None)
         self.bind(Post=self.refresh)
 
     def refresh(self,instance, value):
 
         self.image.texture = self.Post.image.texture
-    #    self.lb_model_year.text = '[b]' + str(self.Post.model) + '[/b]' + ', [size=22]' + str(self.Post.year) + '[/size]'
+        self.lb_model_year.text = '[b]' + str(self.Post.model) + '[/b]' + ', [size=22]' + str(self.Post.year) + '[/size]'
 
     def open_popup(self):
-        p = PostDetailedView(post = self.Post)
+        p = PostDetailedView(post=self.Post)
         p.open()
